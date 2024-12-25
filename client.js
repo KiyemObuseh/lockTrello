5ace31d8988af10eea089d77c1fb8b5a6c004c6f
KiyemObuseh <53870692+KiyemObuseh@users.noreply.github.com>
2024-12-24 23:10:02 -0600

    Update client.js

--- client.js
+++ client.js
@@ -24,6 +24,24 @@ TrelloPowerUp.initialize({
     });
   },
 
+  // Card actions (to restrict adding, moving, or archiving cards in locked lists)
+  'card-buttons': (t) => {
+    return t.card('id', 'idList').then((card) => {
+      return t.get('board', 'shared').then((data) => {
+        const lockKey = `lock_${card.idList}`;
+        if (data[lockKey]) {
+          return [
+            {
+              text: 'Locked',
+              callback: () => t.alert({ message: `This list is locked. You cannot perform this action.` }),
+            },
+          ];
+        }
+        return []; // Allow normal actions if the list is not locked
+      });
+    });
+  },
+
   // Board buttons (optional, for debugging locked lists)
   'board-buttons': (t) => [
     {
@@ -43,4 +61,32 @@ TrelloPowerUp.initialize({
       },
     },
   ],
+});
+
+// Monitor changes to cards and revert actions if necessary
+TrelloPowerUp.initialize({
+  'on-enable': (t) => {
+    return t.get('board', 'shared').then((data) => {
+      const lockedLists = Object.keys(data).filter((key) => key.startsWith('lock_') && data[key]);
+
+      // Add logic to revert changes made to locked lists
+      lockedLists.forEach((listId) => {
+        // Prevent adding new cards
+        t.get('card').then((card) => {
+          if (card.idList === listId) {
+            t.alert({ message: `You cannot add cards to a locked list.` });
+            return t.deleteCard(card.id);
+          }
+        });
+
+        // Prevent moving cards out of the list
+        t.get('card').then((card) => {
+          if (card.idList !== listId && data[`lock_${card.idList}`]) {
+            t.alert({ message: `You cannot move cards out of a locked list.` });
+            t.updateCard({ id: card.id, idList: listId });
+          }
+        });
+      });
+    });
+  },
 });
