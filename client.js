192ff23e97d7bf14dd6c104d5ec3485d620cd898
KiyemObuseh <53870692+KiyemObuseh@users.noreply.github.com>
2024-12-24 23:18:13 -0600

    Update client.js

--- client.js
+++ client.js
@@ -1,23 +1,19 @@
-// Initialize the Power-Up with the required capabilities
 TrelloPowerUp.initialize({
-  // Add actions for list menu
   'list-actions': (t) => {
     return t.list('id', 'name').then((list) => {
-      const listId = list.id;
-
       return [
         {
           text: 'Lock List',
           callback: () => {
-            return t.set('board', 'shared', `lock_${listId}`, true)
-              .then(() => t.alert({ message: `List "${list.name}" has been locked.` }));
+            return t.set('board', 'shared', `lock_${list.id}`, true)
+              .then(() => t.alert({ message: `List "${list.name}" locked.` }));
           },
         },
         {
           text: 'Unlock List',
           callback: () => {
-            return t.set('board', 'shared', `lock_${listId}`, false)
-              .then(() => t.alert({ message: `List "${list.name}" has been unlocked.` }));
+            return t.set('board', 'shared', `lock_${list.id}`, false)
+              .then(() => t.alert({ message: `List "${list.name}" unlocked.` }));
           },
         },
       ];
